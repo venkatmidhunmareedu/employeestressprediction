264cd91156e35760e3f0991facd310b5a8cf7365
M V Midhun <midhunmareedu@gmail.com>
2023-06-16 20:39:32 +0530

    model.py completed

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask , render_template , redirect , url_for , request
+from flask import Flask , render_template , redirect , url_for , request , make_response
 app  = Flask(__name__)
-from model import mul
+from model import model
 
 @app.route("/",methods=['GET'])
 def home():
-    return render_template('home.html')
+    return render_template('home.html' , result=None)
 
-@app.route("/predict" , methods=['POST'])
+@app.route("/predict" , methods=['POST','GET'])
 def predict():
     data = request.form.to_dict()
-    print("Posted data : \n",data)
-    return render_template('home.html', data=data)
+    result = model(data)
+    return render_template('home.html', data=data , result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,2 +1,11 @@
-def mul(a,b):
-    return a * b
+import pickle
+import numpy as np
+
+def model(data):
+    data = np.array(list(data.values()))
+    data = data.astype(float)
+    print(data)
+    LR_model = pickle.load(open('pickel_files/LR.pkl','rb'))
+    data = [data]
+    result = LR_model.predict(data)
+    return result
