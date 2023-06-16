073792c6dcf26bd5be14dbbd1c8353ed08b8ac0c
M V Midhun <midhunmareedu@gmail.com>
2023-06-16 16:52:28 +0530

    add pickel files

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask , render_template , redirect , url_for , request
 app  = Flask(__name__)
-from predict import mul
+from model import mul
 
 @app.route("/",methods=['GET'])
 def home():
